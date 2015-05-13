88306d685db24cf5beb17c2b8b1b6021be8abb78
xuebo.chang <xuebo.chang@langtaojin.com>
2015-05-13 17:01:13 +0800

    add utils
    Change-Id: Ic884add953deeeb56b73e30e9e9c72d6978b14fe

---
+++ app/src/main/java/cn/car4s/app/ui/activity/WebviewActivity.java
@@ -0,0 +1,44 @@
+package cn.car4s.app.ui.activity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import cn.car4s.app.R;
+
+/**
+ * Description:支持js接口调用
+ * Author: Alex
+ * Email: xuebo.chang@langtaojin.com
+ * Time: 2015/5/13.
+ */
+public class WebviewActivity extends BaseActivity implements IBase {
+    @InjectView(R.id.btn_actionbar_back)
+    ImageView btnActionbarBack;
+    @InjectView(R.id.tv_actionbar_title)
+    TextView tvActionbarTitle;
+    @InjectView(R.id.btn_actionbar_conform)
+    ImageView btnActionbarConform;
+    @InjectView(R.id.webview)
+    WebView webview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        ButterKnife.inject(this);
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
